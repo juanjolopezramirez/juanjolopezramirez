839c739e3e19bf2b4bde6f057f8071312c63a6f6
juanjolopezramirez <juanjoselopezramirez@gmail.com>
2024-08-19 14:29:27 -0500

    Language

--- js/main.js
+++ js/main.js
@@ -23,4 +23,22 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
   });
-  
+ 
+  document.getElementById('translateBtn').addEventListener('click', function() {
+    const elements = document.querySelectorAll('.translatable');
+    const currentLang = document.documentElement.lang;
+
+    // Alternar entre idiomas
+    const newLang = currentLang === 'es' ? 'en' : 'es';
+    const newFlagSrc = newLang === 'es' ? './assets/icons/esBtn.svg' : './assets/icons/enBtn.svg';
+    this.src = newFlagSrc; // Cambiar la bandera
+
+    // Cambiar el contenido de la página al nuevo idioma
+    elements.forEach(function(element) {
+        element.textContent = element.getAttribute(`data-${newLang}`);
+    });
+
+    // Actualizar el idioma en el atributo lang del documento
+    document.documentElement.lang = newLang;
+});
+
